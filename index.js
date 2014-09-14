4a44c5c8d804f4a22423b202d8c3abffe15579d2
Jon Abrams <jon@jonabrams.com>
2014-09-13 17:23:07 -0700

    Add .then to cursors

--- index.js
+++ index.js
@@ -127,6 +127,12 @@ Cursor.prototype.size = function(callback) {
 	return this.count(true).nodeify(callback);
 };
 
+// If .then is called on a cursor, invoke toArray() and return promise
+Cursor.prototype.then = function() {
+	var promise = this.toArray()
+	return promise.then.apply(promise, arguments);
+};
+
 
 Cursor.prototype._apply = function(fn, args) {
 	// separate callback and args.
@@ -159,7 +165,6 @@ Cursor.prototype._config = function(fn, args) {
 		return this;
 	}
 };
-
 
 // Proxy for the native collection prototype that normalizes method names and
 // arguments to fit the mongo shell.
